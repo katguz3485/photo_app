3e3a0982a41df1190b6769a3e277b8154ee02c0a
katguz3485 <katarzyna.guzik01@gmail.com>
2018-12-15 14:24:44 +0100

    [Add] new cred typo? final fix?

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,7 @@ else
         provider: 'AWS',
         aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
         aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-        region: Rails.application.credentials.dig(:aws, :region),
+        region: Rails.application.credentials.dig(:aws, :region)
     }
     config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
   end
