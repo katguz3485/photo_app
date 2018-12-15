39330d8a098cdb3d3994745034608166a96623e0
katguz3485 <katarzyna.guzik01@gmail.com>
2018-12-15 13:06:49 +0100

    [Modify] image upload

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,15 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, I18n.t("errors.messages.size_error"))
+    end
+
+  end
 
 end
